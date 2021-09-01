09e0536424f985b2fa0911e23b9cd70edeb355d0
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 23:22:29 -0500

    connected to mongo atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://atlas:atlasagain@cluster0.vqreu.mongodb.net/shopshop?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
